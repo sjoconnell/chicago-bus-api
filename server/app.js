c923223e0382196385e471388795a95ad61224d1
Sam O'Connell <sjoconnell@Sams-MacBook-Pro.local>
2017-04-15 10:14:59 -0500

    adding cors back

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes')
+const cors = require('cors')
 const app = express();
 
 // MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors)
 
 // API
 app.use('/api', routes)
